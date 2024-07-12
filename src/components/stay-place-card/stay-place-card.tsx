b163b8f7bdf82303627b636ff3d948b9dd123d42
lisakolts <crokodile.liza@gmail.com>
2024-07-12 13:09:46 +0300

    4.4 Добавит стейт для компонента Оффера

--- src/components/stay-place-card/stay-place-card.tsx
+++ src/components/stay-place-card/stay-place-card.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Offer } from '../../types/offer.ts';
 
 type StayPlaceCardProps = {
@@ -5,31 +6,45 @@ type StayPlaceCardProps = {
 }
 
 function StayPlaceCard({offer}: StayPlaceCardProps): JSX.Element {
-  const bookmarksClass = offer.isFavorite ?
-    'place-card__bookmark-button button place-card__bookmark-button--active button'
-    : 'place-card__bookmark-button button';
+  const {id, title, type, price, previewImage, isFavorite, isPremium} = offer;
+  const [currentOffer, setCurrentOffer] = useState({
+    id
+  });
+
   return (
-    <article className="cities__card place-card">
-      { offer.isPremium ?
+    <article className="cities__card place-card"
+      id={`offer-${id}`}
+      onMouseOver={() => {
+        setCurrentOffer({
+          ...currentOffer,
+          id: id
+        });
+      }}
+    >
+      { isPremium ?
         <div className="place-card__mark">
           <span>Premium</span>
         </div> : '' }
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src={offer.previewImage} width="260" height="200" alt="Place image" />
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image" />
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{offer.price}</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className={bookmarksClass} type="button">
+          <button className={`place-card__bookmark-button button
+            ${isFavorite ?
+      'place-card__bookmark-button--active'
+      : ''}`} type="button"
+          >
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
-            <span className="visually-hidden">{offer.isFavorite ? 'In bookmarks' : 'To bookmarks'}</span>
+            <span className="visually-hidden">{isFavorite ? 'In bookmarks' : 'To bookmarks'}</span>
           </button>
         </div>
         <div className="place-card__rating rating">
@@ -39,9 +54,9 @@ function StayPlaceCard({offer}: StayPlaceCardProps): JSX.Element {
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">{offer.title}</a>
+          <a href="#">{title}</a>
         </h2>
-        <p className="place-card__type">{offer.type}</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
