85deeb2564d16e51dba6aa9970387e3f51c3285b
lisakolts <crokodile.liza@gmail.com>
2024-08-12 16:21:18 +0300

    8.2.3 Обновление фейворит офферов на странице фейворит оффер пейдж

--- src/components/stay-place-card/stay-place-card-list.tsx
+++ src/components/stay-place-card/stay-place-card-list.tsx
@@ -1,29 +1,29 @@
 import withStayPlaceCard from '../../hocs/with-stay-place-card.tsx';
 import { useAppSelector } from '../../hooks/index.ts';
 import { getOffers } from '../../store/offer-data/selectors.ts';
-import { OfferClick, OfferHover } from '../../types/offer.ts';
+import { Offer, OfferClick, OfferHover } from '../../types/offer.ts';
 import StayPlaceCardItem from './stay-place-card-item.tsx';
 import StayPlaceFavoriteCardItem from './stay-place-favorite-card-item.tsx';
 import { getCurrentCity } from '../../store/city-process/selectors.ts';
-
 const AllStayPlaceCardsWrapped = withStayPlaceCard(StayPlaceCardItem);
 const FavoritesStayPlaceCardsWrapped = withStayPlaceCard(StayPlaceFavoriteCardItem);
 
 type StayPlaceCardListProps = {
+  favoriteOffers?: Offer[];
   className?: string;
   onOfferClick: OfferClick;
   onOfferHover: OfferHover;
   isFavoritePage?: boolean;
 }
 
-function StayPlaceCardList({className, onOfferClick, onOfferHover, isFavoritePage}: StayPlaceCardListProps): JSX.Element {
+function StayPlaceCardList({favoriteOffers, className, onOfferClick, onOfferHover, isFavoritePage}: StayPlaceCardListProps): JSX.Element {
   const currentCity = useAppSelector(getCurrentCity);
   const offers = useAppSelector(getOffers).filter((offer) => offer.city.name === currentCity.name);
 
-  if (isFavoritePage) {
+  if (isFavoritePage && favoriteOffers) {
     return (
       <div className="favorites__places">
-        {offers.map((offer) => <FavoritesStayPlaceCardsWrapped key={offer.id + offer.title} offer={offer} onOfferClick={onOfferClick}/>)}
+        {favoriteOffers.map((offer) => <FavoritesStayPlaceCardsWrapped key={offer.id + offer.title} offer={offer} onOfferClick={onOfferClick}/>)}
       </div>
     );
   }
