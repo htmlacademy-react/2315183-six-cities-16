f09177d533eaaf14e64e37a84b90114f1b706b97
lisakolts <crokodile.liza@gmail.com>
2024-08-12 16:31:20 +0300

    8.2.5 Создание компонента пустой список фейворитс

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -6,8 +6,11 @@ import { useAppSelector } from '../../hooks/index.ts';
 import { Cities } from '../../const.ts';
 import { store } from '../../store/index.ts';
 import { fetchFavoriteOffersAction } from '../../store/api-actions.ts';
-import { getFavoriteOffers } from '../../store/offer-data/selectors.ts';
-import { useEffect } from 'react';
+import { getFavoriteOffers, getOffersDataLoadingStatus } from '../../store/offer-data/selectors.ts';
+import Loader from '../../components/loader/loader.tsx';
+import FavoritesEmptyList from '../../components/favorites-empty-list/favorites-empty-list.tsx';
+
+store.dispatch(fetchFavoriteOffersAction());
 
 type FavoritesPageProps = {
   onOfferClick: OfferClick;
@@ -16,62 +19,71 @@ type FavoritesPageProps = {
 
 function FavoritesPage({onOfferClick, onOfferHover}: FavoritesPageProps): JSX.Element {
   const favoriteOffers = useAppSelector(getFavoriteOffers);
+  const isOffersDataLoading = useAppSelector(getOffersDataLoadingStatus);
 
-  useEffect(() => {
-    store.dispatch(fetchFavoriteOffersAction());
-  }, []);
+  if (isOffersDataLoading) {
+    return <Loader />;
+  }
 
   return (
     <div className="page">
+
       <Helmet>
         <title>6 cities. Favorites</title>
       </Helmet>
       <Header />
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            {
-              favoriteOffers.length !== 0
-                ? (
-                  <ul className="favorites__list">
-                    {
-                      Object.values(Cities).map((city) => {
-                        const favoriteOffersByCity = favoriteOffers.filter((offer) => offer.city.name === city.name);
-                        if (favoriteOffersByCity.length) {
-                          return (
-                            <li className="favorites__locations-items" key={city.name}>
-                              <div className="favorites__locations locations locations--current">
-                                <div className="locations__item">
-                                  <a className="locations__item-link" href="#">
-                                    <span>{city.name}</span>
-                                  </a>
-                                </div>
-                              </div>
-                              <StayPlaceCardList
-                                favoriteOffers={favoriteOffersByCity}
-                                onOfferClick={onOfferClick}
-                                onOfferHover={onOfferHover}
-                                isFavoritePage
-                              />
-                            </li>
-                          );
-                        }
-                      })
-                    }
-                  </ul>
-                )
-                : ''
-            }
-          </section>
-        </div>
-      </main>
-      <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-        </a>
-      </footer>
+
+      {
+        favoriteOffers.length !== 0
+          ? (
+            <>
+              <main className="page__main page__main--favorites">
+                <div className="page__favorites-container container">
+                  <section className="favorites">
+                    <h1 className="favorites__title">Saved listing</h1>
+                    {favoriteOffers.length !== 0
+                      ? (
+                        <ul className="favorites__list">
+                          {Object.values(Cities).map((city) => {
+                            const favoriteOffersByCity = favoriteOffers.filter((offer) => offer.city.name === city.name);
+                            if (favoriteOffersByCity.length) {
+                              return (
+                                <li className="favorites__locations-items" key={city.name}>
+                                  <div className="favorites__locations locations locations--current">
+                                    <div className="locations__item">
+                                      <a className="locations__item-link" href="#">
+                                        <span>{city.name}</span>
+                                      </a>
+                                    </div>
+                                  </div>
+                                  <StayPlaceCardList
+                                    favoriteOffers={favoriteOffersByCity}
+                                    onOfferClick={onOfferClick}
+                                    onOfferHover={onOfferHover}
+                                    isFavoritePage
+                                  />
+                                </li>
+                              );
+                            }
+                          })}
+                        </ul>
+                      )
+                      : ''}
+
+                  </section>
+                </div>
+              </main>
+              <footer className="footer container">
+                <a className="footer__logo-link" href="main.html">
+                  <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
+                </a>
+              </footer>
+            </>
+          )
+          : <FavoritesEmptyList />
+      }
     </div>
   );
 }
+
 export default FavoritesPage;
