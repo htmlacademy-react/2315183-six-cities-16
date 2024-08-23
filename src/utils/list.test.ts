061c65a71a977c741271089911660ddd648573d7
lisakolts <crokodile.liza@gmail.com>
2024-08-23 15:39:14 +0300

    9.1.3 Добавит негатвное тестирование

--- src/utils/list.test.ts
+++ src/utils/list.test.ts
@@ -2,12 +2,20 @@ import { sortOffersPriceLowToHigh } from './list';
 import { makeFakeOffers } from './mocks';
 
 describe('Function: sortOffersPriceLowToHigh', () => {
-  it('should return "true" when its working correct', () => {
+  it('should return "true" when offers differ', () => {
     //Arrange
     const mockOffers = makeFakeOffers();
     //Act
-    const result = mockOffers.sort(sortOffersPriceLowToHigh);
+    const result = mockOffers === mockOffers.sort(sortOffersPriceLowToHigh);
     //Assert
-    expect(result).toBe(typeof mockOffers);
+    expect(result).toBe(true);
+  });
+  it('should return "false" when offers are no differ', () => {
+    //Arrange
+    const mockOffers = makeFakeOffers();
+    //Act
+    const result = mockOffers !== mockOffers.sort(sortOffersPriceLowToHigh);
+    //Assert
+    expect(result).toBe(false);
   });
 });
