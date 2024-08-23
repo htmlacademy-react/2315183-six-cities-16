26d2d84fef4f80587231dd29365ce49009ba45b5
lisakolts <crokodile.liza@gmail.com>
2024-08-23 15:57:58 +0300

    9.1.4 Тестирование оставшихся функций сортировки

--- src/utils/list.test.ts
+++ src/utils/list.test.ts
@@ -1,4 +1,4 @@
-import { sortOffersPriceLowToHigh } from './list';
+import { sortOffersPriceHighToLow, sortOffersPriceLowToHigh, sortTopRatingFirst } from './list';
 import { makeFakeOffers } from './mocks';
 
 describe('Function: sortOffersPriceLowToHigh', () => {
@@ -6,7 +6,8 @@ describe('Function: sortOffersPriceLowToHigh', () => {
     //Arrange
     const mockOffers = makeFakeOffers();
     //Act
-    const result = mockOffers === mockOffers.sort(sortOffersPriceLowToHigh);
+    const sortedMockOffers = mockOffers.sort(sortOffersPriceLowToHigh);
+    const result = mockOffers.every((value, index) => value !== sortedMockOffers[index]);
     //Assert
     expect(result).toBe(true);
   });
@@ -14,7 +15,50 @@ describe('Function: sortOffersPriceLowToHigh', () => {
     //Arrange
     const mockOffers = makeFakeOffers();
     //Act
-    const result = mockOffers !== mockOffers.sort(sortOffersPriceLowToHigh);
+    const sortedMockOffers = mockOffers.sort(sortOffersPriceLowToHigh);
+    const result = mockOffers.every((value, index) => value === sortedMockOffers[index]);
+    //Assert
+    expect(result).toBe(false);
+  });
+});
+
+describe('Function: sortOffersPriceHighToLow', () => {
+  it('should return "true" when offers differ', () => {
+    //Arrange
+    const mockOffers = makeFakeOffers();
+    //Act
+    const sortedMockOffers = mockOffers.sort(sortOffersPriceHighToLow);
+    const result = mockOffers.every((value, index) => value !== sortedMockOffers[index]);
+    //Assert
+    expect(result).toBe(true);
+  });
+  it('should return "false" when offers are no differ', () => {
+    //Arrange
+    const mockOffers = makeFakeOffers();
+    //Act
+    const sortedMockOffers = mockOffers.sort(sortOffersPriceHighToLow);
+    const result = mockOffers.every((value, index) => value === sortedMockOffers[index]);
+    //Assert
+    expect(result).toBe(false);
+  });
+});
+
+describe('Function: sortTopRayingFirst', () => {
+  it('should return "true" when offers differ', () => {
+    //Arrange
+    const mockOffers = makeFakeOffers();
+    //Act
+    const sortedMockOffers = mockOffers.sort(sortTopRatingFirst);
+    const result = mockOffers.every((value, index) => value !== sortedMockOffers[index]);
+    //Assert
+    expect(result).toBe(true);
+  });
+  it('should return "false" when offers are no differ', () => {
+    //Arrange
+    const mockOffers = makeFakeOffers();
+    //Act
+    const sortedMockOffers = mockOffers.sort(sortTopRatingFirst);
+    const result = mockOffers.every((value, index) => value === sortedMockOffers[index]);
     //Assert
     expect(result).toBe(false);
   });
