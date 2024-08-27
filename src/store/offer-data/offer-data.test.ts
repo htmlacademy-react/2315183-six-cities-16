00f0b9541df79774cb291a7ae0df21cc57935001
lisakolts <crokodile.liza@gmail.com>
2024-08-27 17:09:24 +0300

    9.1.7 Добавит тесты слайсов юзер процес и оффера дата

--- src/store/offer-data/offer-data.test.ts
+++ src/store/offer-data/offer-data.test.ts
@@ -1,6 +1,7 @@
 import { changeSort, offerData, resetSort } from './offer-data';
 import { Sorts } from '../../const';
-import { makeFakeOffers } from '../../utils/mocks';
+import { makeFakeCurrentOffer, makeFakeOffer, makeFakeOffers } from '../../utils/mocks';
+import { fetchCurrentOfferAction, fetchFavoriteOffersAction, fetchNearestOfferAction, fetchOffersAction, updateOfferFavoriteStatusAction } from '../api-actions';
 
 describe('OfferData Slice', () => {
   it('should return initial state with empty action', () => {
@@ -77,6 +78,142 @@ describe('OfferData Slice', () => {
     };
 
     const result = offerData.reducer(initialState, resetSort);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "isOffersDataLoading" to "true" with "fetchOffersAction.pending"', () => {
+    const expectedState = {
+      offers: [],
+      favoriteOffers: [],
+      currentOffer: null,
+      nearestOffers: [],
+      sort: Sorts.POPULAR,
+      isOffersDataLoading: true
+    };
+
+    const result = offerData.reducer(undefined, fetchOffersAction.pending);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "offers" to array with offers, "isOffersDataLoading" to "false" with "fetchOffersAction.fulfilled"', () => {
+    const mockOffers = makeFakeOffers();
+    const expectedState = {
+      offers: [mockOffers],
+      favoriteOffers: [],
+      currentOffer: null,
+      nearestOffers: [],
+      sort: Sorts.POPULAR,
+      isOffersDataLoading: false
+    };
+
+    const result = offerData.reducer(undefined, fetchOffersAction.fulfilled(mockOffers, '', undefined));
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "isOffersDataLoading" to "true" with "fetchFavoriteOffersAction.pending"', () => {
+    const expectedState = {
+      offers: [],
+      favoriteOffers: [],
+      currentOffer: null,
+      nearestOffers: [],
+      sort: Sorts.POPULAR,
+      isOffersDataLoading: true
+    };
+
+    const result = offerData.reducer(undefined, fetchFavoriteOffersAction.pending);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "favoriteOffers" to array with favoriteOffers, "isOffersDataLoading" to "false" with "fetchFavoriteOffersAction.fulfilled"', () => {
+    const mockOffers = makeFakeOffers();
+    const expectedState = {
+      offers: [mockOffers],
+      favoriteOffers: [],
+      currentOffer: null,
+      nearestOffers: [],
+      sort: Sorts.POPULAR,
+      isOffersDataLoading: false
+    };
+
+    const result = offerData.reducer(undefined, fetchFavoriteOffersAction.fulfilled(mockOffers, '', undefined));
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "nearestOffers" to array with nearestOffers, "isOffersDataLoading" to "false" with "fetchNearestOfferAction.fulfilled"', () => {
+    const mockOffers = makeFakeOffers();
+    const expectedState = {
+      offers: [mockOffers],
+      favoriteOffers: [],
+      currentOffer: null,
+      nearestOffers: [],
+      sort: Sorts.POPULAR,
+      isOffersDataLoading: false
+    };
+
+    const result = offerData.reducer(undefined, fetchNearestOfferAction.fulfilled(mockOffers, '', ''));
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "isOffersDataLoading" to "true" with "fetchCurrentOfferAction.pending"', () => {
+    const expectedState = {
+      offers: [],
+      favoriteOffers: [],
+      currentOffer: null,
+      nearestOffers: [],
+      sort: Sorts.POPULAR,
+      isOffersDataLoading: true
+    };
+
+    const result = offerData.reducer(undefined, fetchCurrentOfferAction.pending);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "currentOffer" to array with currentOffer, "isOffersDataLoading" to "false" with "fetchCurrentOfferAction.fulfilled"', () => {
+    const mockCurrentOffer = makeFakeCurrentOffer();
+    const expectedState = {
+      offers: [mockCurrentOffer],
+      favoriteOffers: [],
+      currentOffer: null,
+      nearestOffers: [],
+      sort: Sorts.POPULAR,
+      isOffersDataLoading: false
+    };
+
+    const result = offerData.reducer(undefined, fetchCurrentOfferAction.fulfilled(mockCurrentOffer, '', mockCurrentOffer.id));
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "offers" to array with offers, "favoriteOffers" to array with favoriteOffers with "updateOfferFavoriteStatusAction.fulfilled"', () => {
+    const mockOffers = makeFakeOffers();
+    const mockFavoriteOffers = makeFakeOffers();
+    const mockOffer = makeFakeOffer();
+    const mockFavoriteStatus = true;
+
+    const expectedState = {
+      offers: [mockOffers],
+      favoriteOffers: [mockFavoriteOffers],
+      currentOffer: null,
+      nearestOffers: [],
+      sort: Sorts.POPULAR,
+      isOffersDataLoading: false
+    };
+
+    const result = offerData.reducer(undefined, updateOfferFavoriteStatusAction.fulfilled(
+      mockOffers,
+      '',
+      {
+        offer: mockOffer,
+        favoriteStatus: mockFavoriteStatus
+      }
+    ));
 
     expect(result).toEqual(expectedState);
   });
--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -1,6 +1,42 @@
 import { Cities } from '../const';
-import { Offer } from '../types/offer';
+import { CurrentOffer, Offer } from '../types/offer';
 import { datatype, internet, name } from 'faker';
+import { UserData } from '../types/user-data';
+
+export const makeFakeUser = (): UserData => ({
+  email: internet.email(),
+  token: name.title(),
+  name: name.title(),
+  avatarUrl: internet.avatar(),
+  isPro: true
+});
+
+export const makeFakeCurrentOffer = (): CurrentOffer => ({
+  id: name.title(),
+  title: name.title(),
+  type: name.title(),
+  price: datatype.number(),
+  previewImage: internet.avatar(),
+  city: Object.values(Cities)[Math.floor(Math.random() * Object.entries(Cities).length)],
+  location: {
+    latitude: datatype.number(),
+    longitude: datatype.number(),
+    zoom: datatype.number(),
+  },
+  isFavorite: datatype.number() > datatype.number(),
+  isPremium: datatype.number() < datatype.number(),
+  rating: datatype.number(),
+  images: new Array(6).fill(null).map(() => internet.avatar()),
+  description: name.title(),
+  goods: new Array(5).fill(null).map(() => name.title()),
+  host: {
+    name: name.title(),
+    avatarUrl: internet.avatar(),
+    isPro: false,
+  },
+  bedrooms: 2,
+  maxAdults: 2,
+});
 
 export const makeFakeOffer = (): Offer => ({
   id: name.title(),
@@ -17,6 +53,6 @@ export const makeFakeOffer = (): Offer => ({
   isFavorite: datatype.number() > datatype.number(),
   isPremium: datatype.number() < datatype.number(),
   rating: datatype.number(),
-} as Offer);
+});
 
 export const makeFakeOffers = (): Offer[] => new Array(50).fill(null).map(() => makeFakeOffer());
